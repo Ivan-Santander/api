73118112cc176e5646ac9d39a9f3212d0d47da5f
Ivan Santander <ivansantander2020@gmail.com>
2022-04-25 02:11:58 -0500

    initial

---
+++ src/entities/Audit.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn, BaseEntity } from 'typeorm'
+
+@Entity()
+
+export class Audit extends BaseEntity{
+
+    @PrimaryGeneratedColumn()
+    id:number
+    @Column()
+    method:string
+    @Column()
+    status:number
+    @Column()
+    type:string
+    @CreateDateColumn()
+    created_at:Date
+    @UpdateDateColumn()
+    update_ad:Date
+
+}
